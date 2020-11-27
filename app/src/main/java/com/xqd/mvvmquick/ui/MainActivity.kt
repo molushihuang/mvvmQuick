65218102dfb21ef786830b6e3a0ea3117e8ea7b4
xqd <xieqiudongv5@gmail.com>
2020-11-27 17:41:12 +0800

    函数操作符测试

--- app/src/main/java/com/xqd/mvvmquick/ui/MainActivity.kt
+++ app/src/main/java/com/xqd/mvvmquick/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.xqd.mvvmquick.ui
 import android.app.TimePickerDialog
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
@@ -42,6 +43,41 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        //函数式操作符
+        val list = listOf(1, 2, 3, 4, 5, 6, 7)
+//        val listSlice = list.slice(IntRange(1, 3))//2.3.4（传入的是下标起始点和要截取的元素个数）
+        val listSlice = list.slice(listOf(1, 3, 4))//2.4.5（传入的是下标集合）
+        listSlice.forEach { Log.e("slice>", it.toString()) }
+
+        val listFilter = list.filter { it % 2 == 0 }
+        listFilter.forEach { Log.e("filter>", it.toString()) }
+        val listFilterIndex = list.filterIndexed { index, i ->
+            i % 2 == 0 && index < 5
+        }
+        listFilterIndex.forEach { Log.e("filterIndex>", it.toString()) }
+
+        val numberList1 = listOf(23, 65, 14, 57, 99, 123, 26, 15, 88, 37, 56)
+        val numberList2 = listOf(13, 55, 24, 67, 93, 137, 216, 115, 828, 317, 16)
+        val numberList3 = listOf(20, 45, 19, 7, 9, 3, 26, 5, 38, 75, 46)
+        //需要注意一点的是，我们从源码看到filterTo第一个参数destination是一个可变集合类型，所以这里使用的mutableListOf初始化
+        val newNumberList = mutableListOf<Int>().apply {
+            numberList1.filterTo(this) {
+                it % 2 == 0
+            }
+            numberList2.filterTo(this) {
+                it % 2 == 0
+            }
+            numberList3.filterTo(this) {
+                it % 2 == 0
+            }
+        }
+//        newNumberList.sort()
+        newNumberList.forEach {
+            Log.e("filterTo>", it.toString())
+        }
+
+
     }
 
     private fun getTime() {
@@ -58,8 +94,7 @@ class MainActivity : AppCompatActivity() {
 
         //时间选择器
         var timePickerDialog = TimePickerDialog(
-            this,
-            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> },
+            this, { view, hourOfDay, minute -> },
             calendar.get(Calendar.HOUR_OF_DAY),
             calendar.get(Calendar.MINUTE),
             true
@@ -83,5 +118,6 @@ class MainActivity : AppCompatActivity() {
         finish()
         return super.onSupportNavigateUp()
     }
+
 
 }
